eba3fc3b2ca0b7cc6724822ef0bfd1072b7f514f
SANU JOSEPH S <jo.sanu888@gmail.com>
2025-03-13 22:23:28 +0530

    Update sample.py

--- MyApp/sample.py
+++ MyApp/sample.py
@@ -1,10 +1,1 @@
-import google.generativeai as genai
-genai.configure(api_key='AIzaSyD4KzSvbunVkd1TKpmvO83ivvRXk9P0P6E')
-model=genai.GenerativeModel("gemini-pro")
-while True:
-    prompt=input('Ask bot : ')
-    if prompt.lower() not in ['quit', 'exit']:
-        response=model.generate_content(prompt)
-        print(response.text)
-    else:
-        break
+
